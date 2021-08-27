38e3346ff45252a1637919c809ff7d0b7c4e1f9a
HunterD-LevelMax <warzvitaly@gmail.com>
2021-08-27 18:09:30 +0800

    init

--- app/src/main/java/com/template/horoscope/MainActivity.kt
+++ app/src/main/java/com/template/horoscope/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.time.Month
 
 import java.time.format.DateTimeFormatter
 
@@ -20,34 +19,39 @@ class MainActivity : AppCompatActivity() {
         // переход к окну с гороскопом
         fabNext.setOnClickListener {
             if (user == null || user?.age!! <= 0)
-                showToast("Выберите свою дату рождения")
-            else replaceActivity(HoroscopeActivity())
+            else startHoroscope()
+                 replaceActivity(HoroscopeActivity())
         }
         button.setOnClickListener {
-            var day = datePicker.dayOfMonth
-            var month = datePicker.month.plus(1)
-            var year = datePicker.year
-
-            dateTV.text = getBirthdayDate(year, month, day).format(DateTimeFormatter.ofPattern("dd MMMM yyyy"))
-            zodiacTV.text = getZodiac(day, month)
-            nameElementTV.text = getElement(getZodiac(day, month))
-            namePlanetTV.text = getPlanet(getZodiac(day, month))
-            nameYearTV.text = chineseZodiac(year)
-            ageTV.text = getAge(getBirthdayDate(year, month, day)).toString()
-
-
-            //пользователь с данными
-            user = User(
-                getBirthdayDate(year, month, day),
-                getZodiac(day, month),
-                chineseZodiac(year),
-                getElement(getZodiac(day, month)),
-                getPlanet(getZodiac(day, month)),
-                getAge(getBirthdayDate(year, month, day))
-            )
-            saveData(user!!)
+            startHoroscope()
         }
     }
+
+    fun startHoroscope(){
+        var day = datePicker.dayOfMonth
+        var month = datePicker.month.plus(1)
+        var year = datePicker.year
+
+        dateTV.text = getBirthdayDate(year, month, day).format(DateTimeFormatter.ofPattern("dd MMMM yyyy"))
+        zodiacTV.text = getZodiac(day, month)
+        nameElementTV.text = getElement(getZodiac(day, month))
+        namePlanetTV.text = getPlanet(getZodiac(day, month))
+        nameYearTV.text = chineseZodiac(year)
+        ageTV.text = getAge(getBirthdayDate(year, month, day)).toString()
+
+
+        //пользователь с данными
+        user = User(
+            getBirthdayDate(year, month, day),
+            getZodiac(day, month),
+            chineseZodiac(year),
+            getElement(getZodiac(day, month)),
+            getPlanet(getZodiac(day, month)),
+            getAge(getBirthdayDate(year, month, day))
+        )
+        saveData(user!!)
+    }
+
 
     private fun saveData(user: User) {
         val sharedPreferences: SharedPreferences =
@@ -60,7 +64,6 @@ class MainActivity : AppCompatActivity() {
             putString("NAME_YEAR", user?.nameYear)
             putString("BIRTHDAY", user?.birthdayDate.toString())
             putString("AGE", user?.age.toString())
-            showToast(user?.birthdayDate.toString())
         }.apply()
     }
 }
